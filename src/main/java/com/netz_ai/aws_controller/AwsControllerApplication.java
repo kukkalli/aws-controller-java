9fff9963166b48850b995c4a41b5c60609308e17
kukkalli <kukkalli@outlook.com>
2025-11-07 21:49:32 +0100

    Commit the spring initializr as ticker #1

---
+++ src/main/java/com/netz_ai/aws_controller/AwsControllerApplication.java
@@ -0,0 +1,13 @@
+package com.netz_ai.aws_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netz_ai/aws_controller/AwsControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netz_ai.aws_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
